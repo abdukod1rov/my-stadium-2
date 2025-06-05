4d604967725a37ba072e047e7897ce37abde7d14
SAbdukodirov <abdukodirovsamandar3@gmail.com>
2025-06-05 14:22:13 +0500

    remove stadium image router temp

--- app/api/controllers/__init__.py
+++ app/api/controllers/__init__.py
@@ -4,7 +4,7 @@ from .stadium import stadium_router
 from .admin import router as admin_router
 from .booking import booking_router
 from .auth import router as auth_router
-from .stadium_image import stadium_image_router
+# from .stadium_image import stadium_image_router
 
 
 def setup(app: FastAPI) -> None:
@@ -28,7 +28,7 @@ def setup(app: FastAPI) -> None:
         router=admin_router,
         tags=['Admin']
     )
-    app.include_router(
-        router=stadium_image_router,
-        tags=['Stadium Image']
-    )
+    # app.include_router(
+    #     router=stadium_image_router,
+    #     tags=['Stadium Image']
+    # )
